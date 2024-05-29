b878df8e011b3f180104ed625950396aa58a2d38
ChimkenKatsu <tristandominicparajes.202200583@gmail.com>
2024-05-29 00:16:14 -0700

    kulang panag background

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,31 +1,25 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="login-form">
-
-                <form method="POST" action="{{ route('login')}}">
-                    @csrf
-                   <div class="input-group">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter Email" >
-                    </div>
-                    <div class="input-group">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter Password" >
-                    </div>
-                    <div class="input-group">
-                        <button type="submit" href=""class="racetrack-button">Login</button>
-                    </div>
-
-                    <div class="footer">
-                        <a class="text-dark" href="register">Register</a>&nbsp;
-                        &nbsp;
-                        <a  class="text-dark">Forgot Password?</a>&nbsp;
-                    </div>
-                </form>
-            </div>
-
-
-
-
-            @endsection
+<div class="login-form" style="animation: slideInRight 0.5s ease;">
+    <form method="POST" action="{{ route('login')}}">
+        @csrf
+        <div class="input-group">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter Email" >
+        </div>
+        <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter Password" >
+        </div>
+        <div class="input-group">
+            <button type="submit" href=""class="racetrack-button">Login</button>
+        </div>
+        <div class="footer">
+            <a class="text-dark" href="register">Register</a>&nbsp;
+            &nbsp;
+            <a  class="text-dark" href="#">Forgot Password?</a>&nbsp;
+        </div>
+    </form>
+</div>
+@endsection
